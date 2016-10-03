c9bf61f5b560c146e40d13aa76414be2898c1fd5
Jason Mize <jasonemize@gmail.com>
2016-10-03 12:00:03 -0400

    migrations fixed

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -3,23 +3,37 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import django.utils.timezone
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
-                ('text', models.TextField()),
-                ('date_added', models.DateTimeField(default=django.utils.timezone.now, blank=True, null=True, help_text='Posted on:')),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('text', models.CharField(blank=True, max_length=139)),
+                ('date_added', models.DateTimeField(blank=True, help_text='Posted on:', null=True, default=django.utils.timezone.now)),
+                ('user', models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'ordering': ['date_added'],
+                'ordering': ['-date_added'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Ripple',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('original_post', models.ForeignKey(blank=True, null=True, to='users.Post')),
+                ('original_post_owner', models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['original_post'],
             },
         ),
     ]
